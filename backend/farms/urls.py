f6f75a084e3e8c647f40b9314040b8674fbc591a
Venícius <veniciusdobler840@gmail.com>
2026-03-05 17:41:32 -0300

    feat(backend): implement serializers, views and endpoints for AgriculturalInput

--- backend/farms/urls.py
+++ backend/farms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FarmViewSet, CropFieldViewSet
+from .views import FarmViewSet, CropFieldViewSet, AgriculturalInputViewSet
 
 router = DefaultRouter()
 router.register(r'farms', FarmViewSet)
 router.register(r'cropfields', CropFieldViewSet)
+router.register(r'inputs', AgriculturalInputViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
